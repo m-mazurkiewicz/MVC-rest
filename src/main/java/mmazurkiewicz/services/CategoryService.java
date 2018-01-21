c47a9ab8a65f09a9ef6021e01befd97c0ba6ad75
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-01-21 14:43:01 +0100

    add Category Service

---
+++ src/main/java/mmazurkiewicz/services/CategoryService.java
@@ -0,0 +1,12 @@
+package mmazurkiewicz.services;
+
+import mmazurkiewicz.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
