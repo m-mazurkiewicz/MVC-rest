a52f1d35776f85d369c6c93502360c080b56d9d4
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-02-01 12:31:33 +0100

    add MetaData

--- src/main/java/mmazurkiewicz/configuration/SwaggerConfig.java
+++ src/main/java/mmazurkiewicz/configuration/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +23,22 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Marcin M", "www.eloelo.pl", "marcinm@gmail.com");
+
+        return new ApiInfo(
+                "Spring Framework",
+                "Spring Framework5: Beginner to Guru",
+                "1.0",
+                "Terms of service: cos",
+                contact,
+                "Apache License 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
     }
 }
